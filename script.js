959b4a54720da5da0d3420776d4956f33081e9e2
Almaz <almazzzik74@yandex.ru>
2019-11-13 13:26:33 +0300

    Интенсив по trello - v0.2

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document
 		columnElement.setAttribute('data-column-id', columnIdCounter);
 
 		columnElement.innerHTML =
-			`<p class="column-header" contenteditable="true">В плане</p>
+			`<p class="column-header">В плане</p>
 					<div data-notes>
 					</div>
 					<p class="column-footer">
@@ -24,7 +24,9 @@ document
 		document.querySelector('.columns').append(columnElement);
 		columnProcess(columnElement);
 	});
-document.querySelectorAll('.note')
+document
+	.querySelectorAll('.note')
+	.forEach(noteProcess);
 
 function columnProcess(columnElement) {
 	const spanAction__addNote = columnElement.querySelector('[data-action-addNote]');
@@ -37,6 +39,25 @@ function columnProcess(columnElement) {
 		noteIdCounter++;
 
 		columnElement.querySelector('[data-notes]').append(noteElement);
-
+		noteProcess(noteElement);
 	});
+	const headerElement = columnElement.querySelector('.column-header');
+	headerElement.addEventListener('dblclick', function (event) {
+		headerElement.setAttribute('contenteditable', true);
+		headerElement.focus();
+	});
+	headerElement.addEventListener('blur', function (event) {
+		headerElement.removeAttribute('contenteditable');
+	});
+
+}
+
+function noteProcess(noteElement) {
+	noteElement.addEventListener('dblclick', function (event) {
+		noteElement.setAttribute('contenteditable', 'true');
+		noteElement.focus();
+	});
+	noteElement.addEventListener('blur', function (event) {
+		noteElement.removeAttribute('contenteditTable');
+	})
 }
